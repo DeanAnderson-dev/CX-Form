16a2ac3a4fcc074ce6ff0cafe2f980f63bf19499
WiFiWarlord <73312035+DeanAnderson-dev@users.noreply.github.com>
2025-08-07 20:35:51 -0600

    sigpad, drawpad, form data console log
    adding sigpad to page 1, get drawpad on page2 working, update styling. No idea what I'm doing. Some of it works. SigPad does not.

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ canvas.addEventListener("mouseup", () => {
 });
 
 canvas.addEventListener("mousemove", draw);
-
 function draw(e) {
   if (!drawing) return;
 
@@ -34,6 +33,38 @@ function draw(e) {
 
 const clearButton = document.getElementById("clear-btn");
 
+
 clearButton.addEventListener("click", () => {
-  context.clearRect(0, 0, canvas.width, canvas.height);
+ context.clearRect(0, 0, canvas.width, canvas.height);
 });
+
+//form data from page 1 to page 2 //
+    function getData()
+{
+    //gettting the values
+    var fullName = document.getElementById("fullName").value;
+    var address= document.getElementById("address").value; 
+    var address2= document.getElementById("address2").value; 
+    var phone= document.getElementById("phone").value; 
+    var altphone= document.getElementById("altphone").value;
+    var slcnumber= document.getElementById("slcnumber").value;
+
+    //saving the values in local storage
+    localStorage.setItem("txtValue", fullName);
+    localStorage.setItem("txtValue1", address);
+    localStorage.setItem("txtValue2", address2);
+    localStorage.setItem("txtValue3", phone);
+    localStorage.setItem("txtValue4", altphone);
+    localStorage.setItem("txtValue5", slcnumber);    
+}
+
+
+//page 2
+//displaying the value from local storage to another page by their respective Ids
+document.getElementById("data").innerHTML=localStorage.getItem("txtValue");
+document.getElementById("data1").innerHTML=localStorage.getItem("txtValue1");
+document.getElementById("data2").innerHTML=localStorage.getItem("txtValue2");
+document.getElementById("data3").innerHTML=localStorage.getItem("txtValue3");
+document.getElementById("data4").innerHTML=localStorage.getItem("txtValue4");
+document.getElementById("data5").innerHTML=localStorage.getItem("txtValue5");
+
